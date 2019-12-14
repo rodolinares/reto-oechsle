60fba50bf27b8a149390246d266e5a57e4b2da1e
Rodrigo Linares <rodolinares19@gmail.com>
2019-12-14 12:46:18 -0500

    Calcular promedio y desviacion estandar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import DATA from './data';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'reto-oechsle';
+export class AppComponent implements OnInit {
+  average = 0;
+  stDev = 0;
+  sumSqDist = 0;
+  users = DATA;
+
+  constructor() {}
+
+  async ngOnInit() {
+    // Calcular edades
+    this.users.forEach(user => {
+      const birthDate: Date = user.birthDate;
+      const now = new Date();
+      const age = now.getUTCFullYear() - birthDate.getUTCFullYear();
+      user.age = age;
+      this.average += age;
+    });
+
+    // Obtener el promedio
+    this.average /= this.users.length;
+
+    this.users.forEach(user => {
+      this.sumSqDist += Math.pow(Math.abs(user.age - this.average), 2);
+    });
+
+    this.stDev = Math.sqrt(this.sumSqDist / this.users.length);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatListModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
